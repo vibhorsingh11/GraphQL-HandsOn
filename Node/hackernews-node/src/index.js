5a1411c291d0d9d29af4a0ed8acaccfc3248f94d
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-06-08 15:07:03 +0530

    Added mutation

---
+++ Node/hackernews-node/src/index.js
@@ -0,0 +1,41 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+// 2
+let idCount = links.length;
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    // get: (id) => {
+    //   return links.filter((val) => val.id == id);
+    // },
+  },
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
