f1a71d73d3918fc9b7ffb30b02d8e52c235ed793
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-06-08 17:26:50 +0530

    added pagination, filtering and sorting

--- Node/hackernews-node/src/resolvers/Query.js
+++ Node/hackernews-node/src/resolvers/Query.js
@@ -1,7 +1,32 @@
-function feed(parent, args, context, info) {
-    return context.prisma.links()
-  }
-  
-  module.exports = {
-    feed,
-  }
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  });
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count();
+  return {
+    links,
+    count,
+  };
+  // return context.prisma.links()
+}
+
+module.exports = {
+  feed,
+};
